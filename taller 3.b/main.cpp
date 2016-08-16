c0c8706aef3972918b3254da9db72989d739a0f8
camila1998 <camili.andrea1998@gmail.com>
2016-08-16 16:52:32 -0500

    calcular area de triangulo

---
+++ taller 3.b/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    	float alt;
+	float area;
+	float base;
+	cout << "ingrese base" << endl;
+	cin >> base;
+	cout << "ingrese altura" << endl;
+	cin >> alt;
+	area = (base*alt)/2;
+	cout << "la base del triangulo es " << base << " la altura del triangulo es " << alt << endl;
+	cout << "el area del triangulo es " << area << endl;
+	cout << "*\n**\n***\n****\n*****"<<endl;
+	return 0;
+}
+
+
